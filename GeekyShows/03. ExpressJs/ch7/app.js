c387843542ee7af42b86991fc4e7b632240749a2
Bhattii <imran5244513@gmail.com>
2025-01-07 20:48:40 +0500

    controlloer in express

--- GeekyShows/03. ExpressJs/ch7/app.js
+++ GeekyShows/03. ExpressJs/ch7/app.js
@@ -10,12 +10,16 @@ const port = process.env.PORT || '3000';
 // 	res.send(`Student Deleted ${req.params.id}`);
 // });
 
-app.get('/product/:category/:id', (req, res) => {
-	const { category, id } = req.params;
-	res.send(
-		// `Product Category: ${req.params.category}. Product Id: ${req.params.id}`
-		` Product Category: ${category} and Id: ${id}`
-	);
+// app.get('/product/:category/:id', (req, res) => {
+// 	const { category, id } = req.params;
+// 	res.send(
+// 		// `Product Category: ${req.params.category}. Product Id: ${req.params.id}`
+// 		` Product Category: ${category} and Id: ${id}`
+// 	);
+// });
+
+app.get('/student/delete/:id([0-9]{2})', (req, res) => {
+	res.send(`Student Deleted ${req.params.id}`);
 });
 
 app.listen(port, () => {
