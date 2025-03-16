2c1df2883498357fe887917eac3876ce42bcf7fb
Muhammad Imran <imran5244513@gmail.com>
2025-03-16 12:32:11 +0500

    update

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello world") 
+import express from 'express';
+const app = express();  
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.json({ name: 'John Doe', age: 25 });
+}); 
+
+app.get('/about', (req, res) => {
+    res.redirect('/user');
+});
+app.get('/user', (req, res) => {
+    res.send('User page');
+});
+
