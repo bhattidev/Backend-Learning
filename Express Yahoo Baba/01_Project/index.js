e252642993fd655e21c4c088659e23f978307b6e
Muhammad Imran <imran5244513@gmail.com>
2025-03-19 20:45:19 +0500

    update

--- Express Yahoo Baba/01_Project/index.js
+++ Express Yahoo Baba/01_Project/index.js
@@ -1,1 +1,22 @@
-console.log("hello world") 
+import express from 'express';
+const app = express();  
+
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
+app.use(express.json())
+
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.post('/about', (req, res) => {   
+    res.send(req.body.name);
+});
+    
+
