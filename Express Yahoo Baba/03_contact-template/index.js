078e92435f2ab27a04786d0419be45475f53cb34
Muhammad Imran <imran5244513@gmail.com>
2025-04-05 19:16:07 +0500

    update

--- Express Yahoo Baba/03_contact-template/index.js
+++ Express Yahoo Baba/03_contact-template/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import Contact from './models/contacts.models.js';
 const app = express();
 const port = 3000;
 
@@ -8,16 +10,38 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 
+// Database Connection
+mongoose.connect(' mongodb://127.0.0.1:27017/contacts-crud')
+.then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
 
+
+
+// Routes
+app.get('/', async (req, res) => {
+  const contacts = await Contact.find()
+  res.render('home', {contacts})
+});
+
+app.get('/show-contact/:id', async (req, res) => {
+  const contact = await Contact.findById(req.params.id)
+  res.render('show-contact', {contact})
+}); 
+
+app.get('/add-contact', (req, res) => { res.render('add-contact') });
+app.post('/add-contact', (req, res) => {
+  const contact = await Contact.insertOne({
+    
+  })
+  res.send(req.body)
+ });
+
+app.get('/update-contact/:id', (req, res) => { res.render('update-contact')  });
+app.post('/update-contact/:id', (req, res) => {  });
+
+app.get('/delete-contact/:id', (req, res) => { });
+
+// Server Connection
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-// Routes
-app.get('/', (req, res) => { res.render('home') });
-app.get('/show-contact', (req, res) => {res.render('show-contact')});  
-app.get('/add-contact', (req, res) => { res.render('add-contact') });
-app.post('/show-contact', (req, res) => {  res.render('show-contact') });
-app.get('/update-contact', (req, res) => { res.render('update-contact')  });
-app.post('/update-content', (req, res) => { res.render('update-content') });
-app.get('/delete-contact', (req, res) => { res.render('delete-contact') });
