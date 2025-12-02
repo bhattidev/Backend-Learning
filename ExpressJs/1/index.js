7d7ed5ee9dce3476f12076a44a2e129ffd22eb8c
Muhammad Imran <imran5244513@gmail.com>
2025-12-02 06:38:45 +0500

    update

--- ExpressJs/1/index.js
+++ ExpressJs/1/index.js
@@ -1,22 +1,19 @@
-import express from 'express';
+import express from "express";
 const app = express();
 const port = 3000;
+import fs from "fs";
 
-// Get Route
-app.get('/', (req,res)=>{
-    res.send('Hello from Express!')
-})
-
-app.get('/about', (req, res) => {
-  res.send('This is the About Page.');
+const data = fs.readFile("text.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log("2. File contents:", data);
 });
 
-// Post
-app.post('/contact', (req,res)=>{
-res.send('You sent a POST request to /contact');
+// Get Route
+app.get("/", (req, res) => {
+  res.send(data);
 });
 
 // Start the Server
-app.listen(port, ()=>{
-    console.log(`Server is running at http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
