32fe31cd88f96431e513b6851bb27210187d6010
Muhammad Imran <imran5244513@gmail.com>
2026-08-21 21:20:21 +0500

    Implement polls app features: add models, views, templates, and tests for voting functionality

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -1,22 +1,28 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
-def _str_(self):
+
+    def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
     question = models.ForeignKey(
         Question,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
-    def _str_(self):
-        return self.choice_text
-
-
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
