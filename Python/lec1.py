eb92969844e4b26fc3407bfecd6c6807467de477
Muhammad Imran <imran5244513@gmail.com>
2026-08-22 10:11:29 +0500

    Refactor lec1.py: comment out code examples for better readability and organization

--- Python/lec1.py
+++ Python/lec1.py
@@ -1,2 +1,84 @@
-import sys
-print(sys.version)
+# import sys
+# print(sys.version)
+
+# Indentation
+
+# if 5>2:
+#     print('five is greater than two!')
+
+# Variables
+
+# x=5
+# y=7
+# print(x)
+# print(y)
+
+# Print Text
+
+# print("Hello World!")
+# print("I am learning Python.")
+# print("It is awesome!")
+
+# print("Hello World!", end=" ")
+# print("I will print on the same line.")
+
+# Print Numbers
+
+# print(3)
+# print(358)
+# print(50000)
+
+# print(3 + 3)
+# print(2 * 5)
+
+# print("I am", 35, "years old.")
+
+# Python Comments
+
+# print("Hello, World!") #This is a comment
+
+# Variables
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
+
+# Casting
+# x = str(3)    # x will be '3'
+# y = int(3)    # y will be 3
+# z = float(3)  # z will be 3.0
+
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+# Global Variables
+# x = "awesome"
+# def myfunc():
+#     print("Python is " + x)
+# myfunc()
+
+# x = "awesome"
+# def myfunc():
+#     global x
+#     x = "fantastic"
+#     print("Python is " + x)
+# myfunc()
+# print("Python is " + x)
+
+# def myfunc():
+#     global x
+#     x = "fantastic"
+# myfunc()
+# print("Python is " + x)
+
+# Data Types
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
